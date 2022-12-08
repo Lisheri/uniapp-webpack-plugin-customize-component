0e392c5c0c04385eaaa131c2af08ec397ba9e0cd
Lisher <496585582@qq.com>
2022-12-08 17:18:41 +0800

    fix: 修复正则多括号

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# uniapp集成小程序自定义组件
+# uniapp 集成小程序自定义组件
 
-## 允许在uniapp组件级使用小程序原生平台组件
+## 允许在 uniapp 组件级使用小程序原生平台组件
 
 ## 环境
 
 uniapp + webpack
+
+## 支持平台
+
+暂只支持 微信, 字节, qq 和 支付宝, 通过第二个参数区分平台
+
+wx | tt | ax | q
 
 ## 使用
 
@@ -13,9 +19,10 @@ plugins: [
     ...
     new UniappCustomizeComponent({
         'components/audio/index': { 'wx-pay-button': '/wxcomponent/WxPayButton/index' }
-      })
+      }, 'wx')
   ],
 ```
+
 ## 说明
 
-如有疑问, 辛苦提一下issue, 目前仅自测字节和微信小程序, 其余小程序平台未知
+如有疑问, 辛苦提一下 issue, 目前仅自测字节和微信小程序, 其余小程序平台未知
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ type UsingComponents = Record<string, string>;
 
 export type Options = Record<string, UsingComponents>;
 
+export type Platform = "wx" | "tt" | "ax" | "q";
+
 export default class UniappCustomComponent {
   private options = {};
-  constructor(options: Options) {
+  constructor(options: Options, private readonly platform: Platform = "wx") {
     this.options = Object.keys(options).reduce((prev, item) => {
       return {
         ...prev,
         [`${item}.json`]: options[item],
+        [`${item}.${platform}ml`]: options[item],
       };
     }, {});
   }
@@ -19,21 +22,52 @@ export default class UniappCustomComponent {
       "UniappCustomComponent",
       (compilation: any, callback: any) => {
         Object.keys(compilation.assets).forEach((name) => {
+          const currentBundle = compilation.assets[name];
           if (Object.keys(_this.options).includes(name)) {
-            const usingComponentsOption = _this.options[name];
-            const appJson = JSON.parse(compilation.assets[name].source());
-            appJson.usingComponents = {
-              ...(appJson.usingComponents || {}),
-              ...usingComponentsOption,
-            };
-            compilation.assets[name] = {
-              source() {
-                return JSON.stringify(appJson);
-              },
-              size() {
-                return this.source().length;
-              },
-            };
+            if (name.includes("json")) {
+              const usingComponentsOption = _this.options[name];
+              const appJson = JSON.parse(compilation.assets[name].source());
+              appJson.usingComponents = {
+                ...(appJson.usingComponents || {}),
+                ...usingComponentsOption,
+              };
+              compilation.assets[name] = {
+                source() {
+                  return JSON.stringify(appJson);
+                },
+                size() {
+                  return this.source().length;
+                },
+              };
+            } else {
+              // 处理组件标签属性
+              const tags = Object.keys(_this.options[name]);
+              tags.forEach((tag) => {
+                const reg = new RegExp(
+                  `\\<${tag}[\\s\\S]*?(/>|</${tag}>)`,
+                  "g"
+                );
+                compilation.assets[name] = {
+                  source() {
+                    return currentBundle
+                      .source()
+                      .replace(reg, (str: string) => {
+                        if (!str.includes('u-t="m"')) {
+                          // 构建出来没有u-t="m"属性
+                          const targetProps = 'u-t="m"';
+                          const resourcePropsArr = str.split(" ");
+                          resourcePropsArr.splice(2, 0, targetProps);
+                          return resourcePropsArr.join(" ");
+                        }
+                        return str;
+                      });
+                  },
+                  size() {
+                    return this.source().length;
+                  },
+                };
+              });
+            }
           }
         });
         callback();
